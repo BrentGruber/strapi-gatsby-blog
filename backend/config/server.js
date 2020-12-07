4a450767fe44960a24dcd3c47baa1097e9642a83
Brent Gruber <brent.gruber77@gmail.com>
2020-12-07 00:28:33 -0500

    initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f76716a4eaa41323047925f33ea578e2'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3e3a21c3-7706-430e-8e06-b27d4d9c2c7f'
+};
